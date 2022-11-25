26cfdd506ef01ebb6513931895e02dfd859854e0
Josue <josuedilmo@gmail.com>
2022-11-25 17:50:48 +0000

    Map working

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, {enableLatestRenderer} from 'react-native-maps';
+
+//MAP RENDERING
+enableLatestRenderer();
 
 // THIS IS THE STARTING POINT OF THE APP
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="light" />
-
-      <Text style={styles.title}>Mobile Project app!</Text>
-    
-    <View style={styles.card}>
-      <Text style={styles.cardTitle}>This is a Title</Text>
-      <Text style={styles.cardSubtitle}>This is a subtitle</Text>
-    </View>
-    </View>
-
-  );
+    <MapView style={styles.map} />
+  </View>
+);
 }
+
 
 //DEFINE THE STYLES THIS CODE WILL HAVE
 const styles = StyleSheet.create({
@@ -28,26 +25,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  title:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-
-  card:{
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10,
-  },
-
-  cardTitle:{
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-
-  cardSubtitle:{
-    fontSize: 15,
-    color: 'gray',
+  //ABOUT THE MAP
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 
 });
